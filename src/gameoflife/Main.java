8ebc9b7e32b794cbc76be45830091020bfe94ee0
j0mana421 <josselin.marnat@etu.univ-st-etienne.fr>
2015-08-16 20:20:36 +0200

    PUSH

--- src/gameoflife/Main.java
+++ src/gameoflife/Main.java
@@ -3,6 +3,8 @@ package gameoflife;
 public class Main {
 	private static GameWindow gw;
 
+	// TODO PUSH ??
+
 	public static void main(String[] args) {
 		new TranslationHash(TranslationHash.EN);
 		gw = new GameWindow(60, 40, 15);

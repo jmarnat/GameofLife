0dfeb25094abcd82d5613cbfe3c60a79aba2a560
j0mana421 <josselin.marnat@etu.univ-st-etienne.fr>
2015-08-16 20:19:00 +0200

    just a simple change

--- src/gameoflife/Main.java
+++ src/gameoflife/Main.java
@@ -3,8 +3,6 @@ package gameoflife;
 public class Main {
 	private static GameWindow gw;
 
-	// TODO this is not a comment
-
 	public static void main(String[] args) {
 		new TranslationHash(TranslationHash.EN);
 		gw = new GameWindow(60, 40, 15);
